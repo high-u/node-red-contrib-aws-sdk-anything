48b9219042777d393c1300513787e170b7187ae5
high-u <31367724+high-u@users.noreply.github.com>
2017-09-02 14:42:51 +0900

    first

---
+++ aws-sdk-anything.js
@@ -0,0 +1,39 @@
+module.exports = function(RED) {
+    "use strict";
+
+    function AWSSDKAnything(n) {
+        RED.nodes.createNode(this,n);
+        this.awsConfig = RED.nodes.getNode(n.aws);
+        this.region = this.awsConfig.region;
+        this.accessKey = this.awsConfig.accessKey;
+        this.secretKey = this.awsConfig.secretKey;
+        this.service = n.servicename;
+        this.method = n.methodname;
+      
+        var node = this;
+
+      	var AWS = require("aws-sdk");
+        AWS.config.update({
+          accessKeyId: this.accessKey,
+          secretAccessKey: this.secretKey,
+          region: this.region
+        });
+
+        var targetService = new AWS[node.service]();
+
+        node.on("input", function (msg) {
+          node.status({ fill: "blue", shape: "dot", text: "Processing..." });
+          targetService[node.method](msg.payload, function (err, data) {
+            if (err) {
+              node.status({ fill: "red", shape: "dot", text: "error" });
+              node.error("failed: " + err.toString(), msg);
+            } else {
+              node.status({});
+              msg.payload = data;
+              node.send(msg);
+            }
+          });
+      });
+    }
+    RED.nodes.registerType("aws-sdk-anything", AWSSDKAnything);
+};
